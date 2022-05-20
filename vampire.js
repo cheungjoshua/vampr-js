5ab87c19872379169d1e99ea34584ea6143a5785
Joshua Cheung <contact.cjoshua@gmail.com>
2022-05-20 16:06:16 -0700

    miss stretch

--- vampire.js
+++ vampire.js
@@ -10,22 +10,41 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let number = 0;
+    let currentCreator = this;
 
+    while (currentCreator.creator) {
+      currentCreator = currentCreator.creator;
+      number += 1;
+    }
+    return number;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (!vampire.creator) {
+      return false;
+    }
+    if (this.creator === vampire) {
+      return false;
+    }
+    if (!this.creator) {
+      return true;
+    }
+    if (this.creator !== vampire.creator) {
+      return true;
+    }
   }
 
   /** Stretch **/
@@ -35,10 +54,7 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
-  }
+  closestCommonAncestor(vampire) {}
 }
 
 module.exports = Vampire;
-
